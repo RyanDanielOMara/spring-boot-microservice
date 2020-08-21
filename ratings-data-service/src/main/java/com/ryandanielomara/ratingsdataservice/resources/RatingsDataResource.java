46a1f8d4a33de677d9444c39684d77326cca2fdd
Ryan O'Mara <ryandanielomara@gmail.com>
2020-08-21 12:56:50 -0400

    Create all basic services

---
+++ ratings-data-service/src/main/java/com/ryandanielomara/ratingsdataservice/resources/RatingsDataResource.java
@@ -0,0 +1,18 @@
+package com.ryandanielomara.ratingsdataservice.resources;
+
+import com.ryandanielomara.ratingsdataservice.models.Rating;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+    
+}
